99eaf639eba591b0b50e674f7850b1b41d5439be
Bidhu Bhusan Patra <bidhu.patra@outlook.com>
2026-03-12 21:59:52 +0530

    Time: 4 ms (53.66%), Space: 15 MB (18.31%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        int n = nums.size();
-        vector<int> indices;
-        for(int i=0; i<n; i++){
-            for(int j=i; j<n-1; j++){
-                if (nums[i]+nums[j+1] == target){
-                    indices.push_back(i);
-                    indices.push_back(j+1);
-                    break;
-                }
+        unordered_map<int, int> pairIdx;
+
+        for (int i = 0; i < nums.size(); ++i) {
+            int num = nums[i];
+            if (pairIdx.find(target - num) != pairIdx.end()) {
+                return {i, pairIdx[target - num]};
             }
+            pairIdx[num] = i;
         }
-        return indices;
+
+        return {};        
     }
 };
