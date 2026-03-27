6bc847472e49f7109c7cdaaa9dbe8f66f5dd9ac7
Bidhu Bhusan Patra <bidhu.patra@outlook.com>
2026-03-27 12:03:37 +0530

    Time: 0 ms (100%), Space: 11.1 MB (91.44%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -4,16 +4,12 @@ public:
         int n = s.length();
         if (n == 0) return 0;
         int res = 0;
-        for (int i = 0; i < n; i++) {
-            vector<bool> vis(128, false);
-            for (int j = i; j < n; j++) {
-                if (vis[s[j]])
-                    break;
-                else {
-                    res = max(res, j - i + 1);
-                    vis[s[j]] = true;
-                }
-            }
+        vector<int> lastIndex(128, -1); 
+        int start = 0;
+        for (int end = 0; end < n; end++) {
+            start = max(start, lastIndex[s[end]] + 1);
+            res = max(res, end - start + 1);
+            lastIndex[s[end]] = end;
         }
         return res;
     }
