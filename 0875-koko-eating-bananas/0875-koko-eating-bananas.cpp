1876ca5185fdbcad587b237c6b05a52472124379
Bidhu Bhusan Patra <bidhu.patra@outlook.com>
2026-05-14 14:59:55 +0530

    Time: 20 ms (6.74%), Space: 22.9 MB (44.54%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -0,0 +1,20 @@
+#include <cmath>
+
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& piles, int H) {
+        int low = 1, high = 1000000000, k = 0;
+        while (low <= high) {
+            k = low + (high - low) / 2;
+            long long h = 0;
+            for (int i = 0; i < piles.size(); i++) {
+                h += ceil(1.0 * piles[i] / k);
+            }
+            if (h > H)
+                low = k + 1;
+            else
+                high = k - 1;
+        }
+        return low;
+    }
+};
