9656240aafbe861d5b6d67f6a913a778789491ac
Bidhu Bhusan Patra <24110320@outr.ac.in>
2026-05-25 13:04:43 +0530

    Time: 3 ms (69.46%), Space: 38.1 MB (94.61%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.cpp
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -1,8 +1,7 @@
 class Solution {
 public:
     int leastInterval(vector<char>& a, int n) {
-        int f[26]={0}, mx=0, c=0;
-        int s=a.size();
+        int f[26]={0}, mx=0, c=0, s=a.size();
         for(int i=0; i<s; i++) f[a[i]-'A']++;
         for(int i=0; i<26; i++) if(f[i]>mx) mx=f[i];
         for(int i=0; i<26; i++) if(f[i]==mx) c++;
